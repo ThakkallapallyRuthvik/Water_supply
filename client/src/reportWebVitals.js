5aa75db497cfa665f28a6ccd3053facd396686c8
ThakkallapallyRuthvik <146014823+ThakkallapallyRuthvik@users.noreply.github.com>
2023-11-30 18:17:52 +0530

    Ignore reportWebVitals.js

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// Ignore this file
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
