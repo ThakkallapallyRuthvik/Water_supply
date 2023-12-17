35ff675543c2fb4eb925312d7ffb9586a6ba88a4
ThakkallapallyRuthvik <146014823+ThakkallapallyRuthvik@users.noreply.github.com>
2023-12-17 15:02:31 +0530

    Add files via upload

--- client/src/map.js
+++ client/src/map.js
@@ -1,13 +1,15 @@
 import {React,useState,useEffect} from 'react';
 import { useJsApiLoader, GoogleMap, Marker, Polyline, InfoWindow } from "@react-google-maps/api";
-import {Flex,Box,HStack,Text,Button,ButtonGroup,Input} from "@chakra-ui/react";
+import {Flex,Box,HStack,Text,Button,ButtonGroup,Input, position} from "@chakra-ui/react";
 // import {AdvancedMarker,Pin} from '@vis.gl/react-google-maps' 
-
+import GreenHouseMarker from './greenhousemarker.jpeg'
+import RedHouseMarker from './redhousemarker.jpeg'
+import './App.css'
 
 
 function App()
 {
-    const origin = { lat: 17.39763609879129, lng: 78.49017952657705 };
+    const origin = { lat: 16.82365305593255, lng: 78.36275955215761 };
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey:"AIzaSyB6IhzjbQp_KS4lsGdGWq3TvMgMdngijQU",
     })
@@ -16,12 +18,16 @@ function App()
     const [showMarkers, setShowMarkers] = useState(true)
     const [ center, setCenter] = useState(origin)
     const [ selectedMarker, setSelectedMarker] = useState(null)
-    const [ defaultcoordinates, setDefaultCoordinates] = useState([])
-
+    const [ defaultcoordinates, setDefaultCoordinates ] = useState([])
+    const [ housecoords, setHouseCoords ] = useState([])
 
     function addMarker(pos){
-        setCoordinates((prevCoordinates) => [...prevCoordinates, pos]);
+        setCoordinates((prevCoordinates) => [...prevCoordinates, pos])
         
+    }
+
+    function houseMarker(pos){
+        setHouseCoords((prevHouseCoords) => [...prevHouseCoords,pos])
     }
 
     function toggleMarkerVisibility() {
@@ -51,8 +57,9 @@ function App()
             });
             const data = await response.json();
             console.log(data)
-            setCoordinates(data)
-            setDefaultCoordinates(data)
+            setCoordinates(data.coordinates)
+            setDefaultCoordinates(data.coordinates)
+            setHouseCoords(data.housecoords)
           } catch (error) {
             console.error(error);
           }
@@ -70,7 +77,8 @@ function App()
                 'Content-Type':'application/json'
             },
             body: JSON.stringify({
-                coordinates
+                coordinates,
+                housecoords,
             }),
         })
         const data = await response.json()
@@ -84,6 +92,7 @@ function App()
         return "Loading"
     }
     return(
+        <div className='map'>
         <Flex 
         position="relative"
         flexdirection="column"
@@ -91,11 +100,10 @@ function App()
         h="100vh"
         w="100vw"
         backgroundColor="white">
-        <Box position='absolute' left={0} top={80} h="100%" w="100%" >
             {/* Google Map */}
             <GoogleMap 
             center={center} 
-            zoom={15}
+            zoom={17}
             mapContainerStyle={{width:"80%", height:"80%",left:"150px",top:"0"}}
             options={{
                 streetViewControl:false,
@@ -111,22 +119,31 @@ function App()
                 ]
             }}
             onLoad={(map) => setMap(map) }
-            onClick={(event) => addMarker(event.latLng)}>
+            onClick={(event) => addMarker(event.latLng)}
+            onRightClick={(event) => houseMarker(event.latLng)}>
 
 
             {/* Displaying markers */}
             {/* {showMarkers && map && <Marker position={origin} />} */}
-            {showMarkers && coordinates.map((coordinate, index) => (
-                <Marker key={index} position={coordinate} onClick={() => markerIndex(index)} />
-            ))}
+            
+            {housecoords.map((housecoord,index) => (
+                <Marker key={index} position={housecoord} onClick={() => markerIndex(index)} 
+                icon={{
+                    url:GreenHouseMarker,
+                    scaledSize:new window.google.maps.Size(20,20)}} /> 
+                ))}
+
+            {/* {showMarkers && coordinates.map((coordinate, index) => (
+                <Marker key={index} position={coordinate} />
+            ))} */}
             
             {/* Draw polyline */}
-            {coordinates.length>1 && <Polyline path={coordinates}/>}
+            {coordinates.length>1 && <Polyline path={coordinates} options={{strokeColor:"deepskyblue",strokeOpacity:1}} />}
 
             {/* Display InfoWindow when a marker is selected */}
             {selectedMarker !== null && (
                 <InfoWindow
-                position={coordinates[selectedMarker]}
+                position={housecoords[selectedMarker]}
                 onCloseClick={() => setSelectedMarker(null)} // Close InfoWindow when clicked
                 >
                 {/* Add your content for the InfoWindow */}
@@ -139,7 +156,6 @@ function App()
 
 
             </GoogleMap> 
-            </Box>
         <Box
         position='absolute'
         p={4}
@@ -149,30 +165,35 @@ function App()
         shadow='base'
         minW='container.md'
         zIndex='modal'
-        w='40%'
+        w='50%'
         h='7%'
         top="5%"            
-        left="70%"           
+        left="65%"           
         transform="translate(-50%, -50%)"
         >
         <HStack spacing={4}>
         <ButtonGroup height="100%">
+            <Button bgColor="white" position="absolute" type="button" height="50%" 
+            width="20%" >
+            Draw new line
+            </Button>
             <Button bgColor="white" position="absolute" type="button" height="50%"
-             width="30%" onClick={toggleMarkerVisibility}>
+             width="20%" left="20%" onClick={toggleMarkerVisibility}>
             {showMarkers ? 'Hide Markers' : 'Show Markers'}
             </Button>
             <Button bgColor="white" position="absolute" type="submit" 
-            left="30%" height="50%" width="30%" onClick={deleteLastLine}>
+            left="40.5%" height="50%" width="20%" onClick={deleteLastLine}>
             Delete last line
             </Button>
             <Button bgColor="white" position="absolute" type="submit" 
-            left="61%" height="50%" width="30%" onClick={submitCoordinates}>
+            left="61%" height="50%" width="20%" onClick={submitCoordinates}>
             Submit
             </Button>
         </ButtonGroup>
         </HStack>
         </Box>
         </Flex>
+        </div>
     )
 }
 
